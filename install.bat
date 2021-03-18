e4c2236989ede8732662fd12411ace52d50a19de
cmdcustom <76268123+cmdcustom@users.noreply.github.com>
2021-03-17 18:30:11 -0600

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,6 @@
+@echo off
 echo Run as admin
+set /p dir= "Directory: "
 
-move src\gitr.bat %windir%\System32
-move src\gitr-remove.bat %windir%\System32
+move %dir%\src\gitr.bat %windir%\System32
+move %dir%\src\gitr-remove.bat %windir%\System32
