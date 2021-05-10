3a0ea9e80bc5409714c4c06997caaa5830fc5052
cmdcustom <76268123+cmdcustom@users.noreply.github.com>
2021-05-10 11:03:47 -0600

    Create gitr-noremove.bat

---
+++ src/gitr-noremove.bat
@@ -0,0 +1,11 @@
+@echo off
+
+for /f "tokens=1 delims=/" %%i in ('echo %1') DO set owner=%%i
+for /f "tokens=2 delims=/" %%i in ('echo %1') DO set repo=%%i
+
+powershell -command iwr https://api.github.com/repos/%owner%/%repo%/zipball -outfile c.zip
+powershell -command expand-archive -path c.zip -destinationpath .\repoexpanded
+
+del c.zip
+
+@echo on
